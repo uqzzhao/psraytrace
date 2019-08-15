da50289f37063bf19e9e3183bb8243ec32fc33a8
unknown <zhengguang.zhao@uq.net.au>
2019-08-15 18:33:07 +1000

    example_vert_hor.py added

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from examples import example2d, example3d, example_vert_hor
 
 def main():
 
-    # example2d.run() # a downhole monitoring example
-    # example3d.run() # a surface monitoring example
-    example_vert_hor.run() # src and rcv pairs
+    example2d.run() # a downhole monitoring example
+    example3d.run() # a surface monitoring example
+    example_vert_hor.run() # src and rcv pairs for shooting vertically and horizontally
 
 
 # This will actually run the code if called stand-alone:
